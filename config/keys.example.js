1796c516607f75df82da8a157b10bc2e3423656b
jerioz <escobedoranero@gmail.com>
2023-05-29 21:49:47 +0200

    update end.example

--- config/keys.example.js
+++ config/keys.example.js
@@ -1,4 +1,4 @@
 module.exports = {
     MONGO_URI: 'mongodb+srv://<user>:<password>@cluster0.bq6byot.mongodb.net/socialDB?retryWrites=true&w=majority',
-    jwt_secret: 'tu secretito'
+    JWT_SECRET: 'tu secretito'
 }
